7bad8c0ffd931f92e4f040437566152b43137e05
Gokul D <gokul.d@juspay.in>
2026-08-05 18:03:29 +0530

    fix: change import order in content
    - changed the import order

--- src/remote/content.ts
+++ src/remote/content.ts
@@ -2,8 +2,8 @@ import type { APIResponse } from "typesafe-api-call";
 import type { Executor } from "./executor";
 import {
   type Page,
-  type PageConnection,
   type Blog,
+  type PageConnection,
   type BlogConnection,
   type Article,
   type ArticleConnection,
